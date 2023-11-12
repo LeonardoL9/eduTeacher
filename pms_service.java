b3ac561c0327d57065e8dcbcc83f541ddc569fda
LeonardoL9 <114910367+LeonardoL9@users.noreply.github.com>
2023-11-12 16:26:57 +0800

    Add files via upload

---
+++ pms_service.java
@@ -0,0 +1,11 @@
+package service;
+
+public interface pms_service {
+    public void registration();
+    void personChange();
+    void selectById();
+    void selectAll();
+    void removeById();
+    void updateById();
+
+}
